b8964f1a010dc991bf50d27040d44ed65920ec0c
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2021-05-18 22:12:38 +0800

    Race Condition

---
+++ src/main/java/com/kensbunker/multithreading/LongWrapper.java
@@ -0,0 +1,18 @@
+package com.kensbunker.multithreading;
+
+public class LongWrapper {
+
+  private long l;
+
+  public LongWrapper(long l) {
+    this.l = l;
+  }
+
+  public long getValue() {
+    return l;
+  }
+
+  public void incrementValue() {
+    l = l + 1;
+  }
+}
